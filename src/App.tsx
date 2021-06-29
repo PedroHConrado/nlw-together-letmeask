5671a767c2c6a391f3448eecf90b93bf7e640ad5
Pedro Henrique Conrado <conradohpedro@gmail.com>
2021-06-29 11:03:03 -0300

    add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+import { AuthContextProvider } from './contexts/AuthContext'
+import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
+
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
+        </Switch>
+      </AuthContextProvider>
+
+    </BrowserRouter>
   );
 }
 
